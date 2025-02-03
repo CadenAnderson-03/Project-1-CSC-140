8449829d463ee96109f414e97b919a42d2871b67
CadenAnderson-03 <andersonc1230@outlook.com>
2025-02-02 20:22:44 -0600

    Initial commit

---
+++ CSC-140-Project-1.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>  
+
+int main() {
+    
+    int myInt = 123;
+    float myFloat = 2.5;
+    double myDouble = 1.23456;
+    char myChar = 'A';
+    bool myBool = true;
+    std::string myString = "Hello, World!";
+
+    
+    std::cout << "Integer: " << myInt << std::endl;
+
+    std::cout << "Float: " << myFloat << std::endl;
+
+    std::cout << "Double: " << myDouble << std::endl;
+
+    std::cout << "Character: " << myChar << std::endl;
+
+    std::cout << "Boolean: " << myBool << std::endl;
+    
+    std::cout << "String: " << myString << std::endl;
+
+    return 0;
+}
