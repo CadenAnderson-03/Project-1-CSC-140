e75a8f5ea38f05eba987a43c101d558003bc8f45
CadenAnderson-03 <andersonc1230@outlook.com>
2025-02-20 20:34:49 -0600

    Assignment 2
    Here is assignment 2, which involves basic arithmetic.

---
+++ BasicArithmetic.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+int main() {
+    
+    // Declaring my two integer variables
+    int num1 = 15;
+
+    int num2 = 2;
+
+    // The arithmetic operations to do with the two integers
+    std::cout << num1 << " + " << num2 << " = " << (num1 + num2) << std::endl;
+
+    std::cout << num1 << " - " << num2 << " = " << (num1 - num2) << std::endl;
+
+    std::cout << num1 << " * " << num2 << " = " << (num1 * num2) << std::endl;
+
+    std::cout << num1 << " / " << num2 << " = " << (num1 / num2) << std::endl;
+
+    std::cout << num1 << " % " << num2 << " = " << (num1 % num2) << std::endl;
+
+    return 0;
+}
