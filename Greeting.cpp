c52b26c8d92c57a330792588afc762500f8f15c4
CadenAnderson-03 <andersonc1230@outlook.com>
2025-02-21 18:40:17 -0600

    Assignment 3
    This is the greeting code for assignment 3.

---
+++ Greeting.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+int main() {
+
+    std::string name;
+    
+    // Prompting my name here
+
+    std::cout << "Enter your name: Caden \n ";
+ 
+    // Here I am greeting myself
+
+    std::cout << "It is nice to meet you, Caden! " << std::endl;
+
+    return 0;
+}
